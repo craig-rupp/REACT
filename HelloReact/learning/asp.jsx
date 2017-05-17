e896029e711703d1076a2faf3c51ae8bca4cb8b9
Craig Rupp <craigrupp@utexas.edu>
2017-05-17 18:47:26 -0500

    getDefaultProps function and passing variable through JS expressions

--- HelloReact/learning/asp.jsx
+++ HelloReact/learning/asp.jsx
@@ -1,15 +1,30 @@
 var Greeter = React.createClass({
+	getDefaultProps : function(){
+		return {
+			name : "React",
+			message : "Default message should the message variable be blank"
+		};
+	},
 	render : function(){
+		// return React.createElement(
+		// 	'h1',
+		// 	null,
+		// 	'Hello React.createElement'
+		// );
+		var name = this.props.name;
+		var message = this.props.message;
 		return (
 			<div>
-			<h1>Hello React! Arsenal</h1>
+			<h1>Hello {name}! Arsenal</h1>
 			<p>They beat <strong>United</strong> yesterday</p>
+			<p>Detailed message contains <strong>{message}</strong></p>
 			</div>
 		);
 	}
 });
-
+var firstName = 'Mesut';
+var Arsenal = 'With 75 points Arsenal could still find themselves out of the UCL equation for 2017, unless Liverpool shit the bed';
 ReactDOM.render(
-  <Greeter/>,
+  <Greeter name={firstName} message={Arsenal}/>,
   document.getElementById('app')
 );
